b181a0fd8321bb342252eaf0d68103ae743b4cc7
TMN <t.martin@adsoftware.fr>
2020-09-14 21:46:18 +0200

    mieux ?

--- pages/[concertDate].js
+++ pages/[concertDate].js
@@ -4,13 +4,13 @@ import { DateTime, Settings } from "luxon";
 Settings.defaultLocale = "fr";
 
 export default function concertDate({ concerts }) {
-  // const { artist, startDate, venue } = concerts[0];
+  const { artist, startDate, venue } = concerts[0];
 
   return (
     <div>
-      {/* <h1>{DateTime.fromISO(startDate).toFormat("dd-LL-yyyy")}</h1>
+      <h1>{DateTime.fromISO(startDate).toFormat("dd-LL-yyyy")}</h1>
       <p>Artist : {artist.name}</p>
-      <p>Salle : {venue.name}</p> */}
+      <p>Salle : {venue.name}</p>
     </div>
   );
 }
@@ -31,15 +31,15 @@ export async function getStaticPaths() {
 }
 
 export const getStaticProps = async ({ params: { concertDate } }) => {
-  // const res = await fetch(`${rootUrl}/api/concerts`, {
-  //   method: "POST",
-  //   headers: {
-  //     Accept: "application/json",
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify({ concertDate }),
-  // });
-  // const concerts = await res.json();
+  const res = await fetch(`${rootUrl}/api/concerts`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ concertDate }),
+  });
+  const concerts = await res.json();
   const concerts = [];
   return { props: { concerts } };
 };
