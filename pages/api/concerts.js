d1c2883c2d5231a04fe4ba723d2cc01d759f883d
TMN <tommymartin1234@gmail.com>
2021-04-30 14:40:23 +0200

    fix?

--- pages/api/concerts.js
+++ pages/api/concerts.js
@@ -10,24 +10,32 @@ handler.use(middleware);
 
 //get all concerts
 handler.get(async (req, res) => {
-  const concerts = await req.db.collection("concerts").find().toArray();
-  res.json(concerts);
+  try {
+    const concerts = await req.db.collection("concerts").find().toArray();
+    res.json(concerts);
+  } catch (error) {
+    console.error(error?.message || error);
+  }
 });
 
 //get concerts for a date
 handler.post(async (req, res) => {
-  const date = req.body.concertDate;
-  const concerts = await req.db
-    .collection("concerts")
-    .find({
-      startDate: {
-        $gte: DateTime.fromFormat(date, ddLLyyyy).startOf("day"),
-        $lt: DateTime.fromFormat(date, ddLLyyyy).endOf("day"),
-      },
-    })
-    .sort({ date: 1 })
-    .toArray();
-  res.json(concerts);
+  try {
+    const date = req.body.concertDate;
+    const concerts = await req.db
+      .collection("concerts")
+      .find({
+        startDate: {
+          $gte: DateTime.fromFormat(date, ddLLyyyy).startOf("day"),
+          $lt: DateTime.fromFormat(date, ddLLyyyy).endOf("day"),
+        },
+      })
+      .sort({ date: 1 })
+      .toArray();
+    res.json(concerts);
+  } catch (error) {
+    console.error(error?.message || error);
+  }
 });
 
 export default handler;
