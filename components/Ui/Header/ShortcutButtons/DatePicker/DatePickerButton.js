f7c30112b4bf2b5a76233ad28481dd84325f3ceb
Eve-Anne <eve-anne.rousselin.simplon@gmail.com>
2021-05-11 13:05:16 +0200

    button

--- components/Ui/Header/ShortcutButtons/DatePicker/DatePickerButton.js
+++ components/Ui/Header/ShortcutButtons/DatePicker/DatePickerButton.js
@@ -2,6 +2,7 @@ import React from "react";
 import DatePicker from "./DatePicker";
 import fr from "date-fns/locale/fr";
 import { registerLocale } from "react-datepicker";
+
 registerLocale("fr", fr);
 
 export default function DatePickerButton({ date }) {
--- components/Ui/Header/ShortcutButtons/TodayButton.js
+++ components/Ui/Header/ShortcutButtons/TodayButton.js
@@ -10,7 +10,7 @@ const SuperCoolButton = styled.button`
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
     "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji",
     "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-  padding: 12px;
+  padding: 8px;
   border: 4px solid #f4e9da;
   background-color: #273253;
   color: #f4e9da;
